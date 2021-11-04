5cfd664b9993e8823a947009de4b274c7567f2ed
Francisco del Real Escudero <soraquico@gmail.com>
2021-11-03 21:12:26 -0500

    NEW: Added file structure with Presto and Kafka configuration.

--- script.sh
+++ script.sh
@@ -6,39 +6,6 @@ eval "wget https://repo1.maven.org/maven2/com/facebook/presto/presto-server/0.26
 eval "tar -xvzf presto-server-0.264.1.tar.gz && mv presto-server-0.264.1 presto-server"
 eval "mkdir data"
 eval "cd presto-server"
-eval "mkdir etc"
-eval "cd etc"
-
-eval "echo \"coordinator=true
-node-scheduler.include-coordinator=true
-http-server.http.port=8080
-query.max-memory=5GB
-query.max-memory-per-node=1GB
-query.max-total-memory-per-node=2GB
-discovery-server.enabled=true
-discovery.uri=http://localhost:8080\" > config.properties"
-
-eval "echo \"-server
--Xmx16G
--XX:+UseG1GC
--XX:G1HeapRegionSize=32M
--XX:+UseGCOverheadLimit
--XX:+ExplicitGCInvokesConcurrent
--XX:+HeapDumpOnOutOfMemoryError
--XX:+ExitOnOutOfMemoryError
--Djdk.attach.allowAttachSelf=true\" > jvm.config"
-
-eval "echo \"node.environment=test
-node.id=ffffffff-ffff-ffff-ffff-ffffffffffff
-node.data-dir=../data\" > node.properties"
-
-eval "mkdir catalog"
-eval "echo \"connector.name=kafka
-kafka.nodes=localhost:9092
-kafka.table-names=tpch.customer,tpch.orders,tpch.lineitem,tpch.part,tpch.partsupp,tpch.supplier,tpch.nation,tpch.region
-kafka.hide-internal-columns=false\" > catalog/kafka.properties"
-
-eval "cd ../.."
 
 # Presto CLI
 eval "wget -O presto https://repo1.maven.org/maven2/com/facebook/presto/presto-cli/0.264.1/presto-cli-0.264.1-executable.jar"
